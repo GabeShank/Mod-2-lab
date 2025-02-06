67cac65d0d82a03561760e3da3cb5a95d2dec32b
GabeShank <gshank@mail.pima.edu>
2025-02-06 15:11:43 -0700

    Update cis129_lab02_shortStory.py
    short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,2 +1,9 @@
 print('Hello, World!') 
-print('short story')
+# Author: Gabriel Shank
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('A man has woken up in a leveled city with no one around and no explanation for what has happened. ')
+print('He comes to find one building in perfect condition, a vast library, and yet still no one is around. ')
+print('He realizes that his only form of entertainment are these books and plans on using the rest of his days in the pursuit of knowledeg.')
+print('He trips and falls and drops his glasses, while looking for them he hears a crunch and knows his fate has been sealed')
+print('A man now with no glasses, no one to talk too, and no way of reading is now found with many years ahead of him and nothing to do with them')
